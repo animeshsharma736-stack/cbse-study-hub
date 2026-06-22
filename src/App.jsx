1f9493e5f6ad4db16231bc81a4343afbbb5a28d3
animeshsharma736-stack <animesh.sharma736@gmail.com>
2026-06-22 19:14:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from '@/context/AuthContext'
 import { Toaster } from 'sonner'
 import Layout from '@/components/Layout'
